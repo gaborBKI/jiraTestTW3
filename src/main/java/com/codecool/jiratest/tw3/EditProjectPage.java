43bcba20ed8136c7d159505fd4bb7245484a3fb4
erika <erikatoth.0831@gmail.com>
2019-07-23 11:04:15 +0200

    Edit page opens test

--- src/main/java/com/codecool/jiratest/tw3/EditProjectPage.java
+++ src/main/java/com/codecool/jiratest/tw3/EditProjectPage.java
@@ -1,4 +1,32 @@
 package com.codecool.jiratest.tw3;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class EditProjectPage {
+
+    private Util util;
+
+    @FindBy(id="edit-issue")
+    private WebElement editButton;
+
+    @FindBy(id="edit-issue-dialog")
+    private WebElement editDialogBox;
+
+
+    public EditProjectPage(WebDriver driver) {
+        util = new Util(driver);
+    }
+
+    public void clickEdit() {
+        util.waitFor(editButton);
+        editButton.click();
+    }
+
+    public boolean verifyEditButton(){
+        util.waitFor(editDialogBox);
+        return editDialogBox.isDisplayed();
+    }
+
 }
--- src/test/java/EditIssueTest.java
+++ src/test/java/EditIssueTest.java
@@ -1,24 +1,27 @@
-import com.codecool.jiratest.tw3.DashboardPage;
+import com.codecool.jiratest.tw3.EditProjectPage;
 import com.codecool.jiratest.tw3.LoginPage;
 import com.codecool.jiratest.tw3.Navigate;
-import org.junit.Assert;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.junit.Assert;
 
-public class LoginTest {
+public class EditIssueTest {
 
     private static WebDriver driver;
     private static Navigate navigate;
     private static LoginPage loginPage;
-    private static DashboardPage dashBoardPage;
+    private static EditProjectPage objEditProjectPage;
+
 
     @BeforeAll
-    public static void init(){
+    public static void setUp(){
         switch (System.getenv("driverType")){
             case "Chrome":
                 driver = new ChromeDriver();
@@ -28,21 +31,29 @@ public class LoginTest {
                 break;
         }
         loginPage = PageFactory.initElements(driver, LoginPage.class);
-        dashBoardPage = PageFactory.initElements(driver, DashboardPage.class);
+        objEditProjectPage = PageFactory.initElements(driver, EditProjectPage.class);
         navigate = new Navigate(driver);
         navigate.toLoginPage();
+        loginPage.userLogin(System.getenv("JIRAUSER"), System.getenv("PASSWORD"));
     }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/urlList.csv", numLinesToSkip = 1)
+    public void editPageOpensTest(String url) {
+        driver.navigate().to(url);
+        objEditProjectPage.clickEdit();
+        Assert.assertTrue(objEditProjectPage.verifyEditButton());
+    }
+
+    @Test
+    public void inlineEditing() {
+
+    }
+
 
     @AfterAll
     public static void tearDown(){
         driver.close();
     }
-
-    @Test
-    public void happyPathTest(){
-        loginPage.userLogin(System.getenv("JIRAUSER"), System.getenv("PASSWORD"));
-        Assert.assertTrue(dashBoardPage.verifyUserButton());
-    }
-
 
 }
